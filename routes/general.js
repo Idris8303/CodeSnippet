c9fa45679497475dd3d6266deb88f7d1b93025e8
Idris8303 <bctxtoast@gmail.com>
2017-11-16 17:57:28 -0600

    view snippets mods

--- routes/general.js
+++ routes/general.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bodyParser = require('body-parser');
 const Snippet = require('../models/snippet');
 
 router.get('/', (req, res) => {
@@ -24,11 +25,25 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
-router.get('/search', function (req, res) {
-  res.render('search');
-});
+router.get('/searchSnippets/:searchBy?/:searchTerm?', (req, res) => {
+    let searchBy = req.params.searchBy;
+    let searchTerm = req.params.searchTerm;
+    let object = {};
+    if (searchBy && searchTerm) {
+      object[searchBy] = searchTerm;
+    }
+  Snippet.find(object, (err, snippets) => {
+    if(err) {
+      req.flash('Sorry.There was a problem saving your snippet.');
+    } else {
+      let data = {snippets};
 
-router.post('/search', (req, res) => {
+      console.log(data);
+    res.render('snippets',data)
+    }
+  })
+});
+router.post('/searchSnippetsBy/:searchBy?/:searchTerm?', (req, res) => {
 
     let searchBy = req.params.searchBy;
     let searchTerm = req.params.searchTerm;
@@ -38,11 +53,11 @@ router.post('/search', (req, res) => {
     }
   Snippet.find(object, (err, snippets) => {
     if(err) {
-      req.flash('Sorry.There was a problem saving.');
+      req.flash('there was a problem in saving your snippet.');
     } else {
-
-      console.log(snippets);
-    res.render('search',{searchResults: snippets})
+      let data = {snippets};
+      console.log(data);
+    res.render('snippets', data)
     }
   })
 });
@@ -67,24 +82,37 @@ router.post('/create', (req, res) => {
   });
   res.redirect('create');
 });
+//   app.get('/snippets/:snippetId',function(req,res){
+// })
 
-app.get('/snippets/:snippetId', function (req, res) {
-  // create snippet mustache template
-  // get the snppet with id == snippetId
-  // and render it in the snippet mustache template
-  res.send(req.params.snippetId)
+// app.get('/snippets/:snippetId', function (req, res) {
+//   // create snippet mustache template
+//   // get the snppet with id == snippetId
+//   // and render it in the snippet mustache template
+//   res.send(req.params.snippetId)
+// })
+
+// router.get('/snippets/', (req, res) => {
+//   console.log(req.user);
+//   Snippet
+//     .find({})
+//     .then((codeSnippets) => {
+//       res.render('snippets', {
+//         codeSnippets
+//       });
+//     });
+// });
+router.get('/findASnippetbyTitle', (req, res) => {
+let findByTitle = Snippet.find(findSnippetByTitle, searchQuery);
+ console.log(JSON.stringify(findByTitle));
+
+function findSnippetByTitle (findByTitle) {
+  if (`findByTitle.${searchQuery}` === `req.body.${searchQuery}`) {
+    let returnedSnippet = {snippets};
+    res.render('snippets', returnedSnippet);
+  }
+  }
 })
-
-router.get('/snippets/', (req, res) => {
-  console.log(req.user);
-  Snippet
-    .find({})
-    .then((codeSnippets) => {
-      res.render('snippets', {
-        codeSnippets
-      });
-    });
-});
 
 
 module.exports = router;
